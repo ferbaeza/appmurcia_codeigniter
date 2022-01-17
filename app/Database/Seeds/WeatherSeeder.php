807b8afe8aed84b8396bf813a98953024d0ee3c1
Alfonso <alfonsopm08@gmail.com@mail.com>
2022-01-17 19:50:12 +0100

    Seeders acabados

--- app/Database/Seeds/WeatherSeeder.php
+++ app/Database/Seeds/WeatherSeeder.php
@@ -9,6 +9,22 @@ class WeatherSeeder extends Seeder
 {
     public function run()
     {
-        //
+        $this->db->table('weather')->where("id >", 0)->delete();
+        $this->db->query("ALTER TABLE weather AUTO_INCREMENT=1");
+
+        $created= new Time('-4 days');
+        $updated= new Time();
+        $weather=[
+            [
+                'main' => 'Soleado',
+                'description' => 'Soleado sin nubes',
+                'icon' => 'https://cdn.icon-icons.com/icons2/33/PNG/256/sunny_sunshine_weather_2778.png',
+                'created_at' =>$created->format('Y-m-d H:i:s'),
+                'updated_at' =>$updated->format('Y-m-d H:i:s'),
+            ],
+           
+        ];
+        $builder = $this->db->table('weather');
+        $builder->insertBatch($weather);
     }
 }
